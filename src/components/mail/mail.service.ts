a9e099053dda573b7f2b981dede8f74bcf2f5b66
Rajat <rajat.verma@effectualservices.in>
2024-05-13 10:07:39 +0530

    code

--- src/components/mail/mail.service.ts
+++ src/components/mail/mail.service.ts
@@ -14,7 +14,7 @@ export class MailService {
   ): any {
     this.mailerService
       .sendMail({
-        to: 'rajat.verma@effectualservices.in',
+        to: 'satya.tyagi@effectualservices.com',
         from: 'eksuser@effectualservices.in',
         subject: 'IP Academy Result',
         template: 'examComplete',
